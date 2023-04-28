cd391a998a557080d3e772b8d4780b4ac3e2bd0c
Pollo <nilton_pv@hotmail.com>
2023-04-27 20:02:06 -0500

    Agregando funcionalidad a la página con javaScript (alerta en el textarea)

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
-//alert("Hola mundo!!")
+//variables
+const writeData = document.querySelector('#write-data');
+const regex = /^[a-z ]*$/;  //expresión re. solo acepta letras no mayúsculas
+
+writeData.addEventListener('keyup', alertMensaje);
+
+//funciones
+
+function alertMensaje() {
+
+    if( regex.exec(writeData.value) ) {
+
+        document.querySelector('.form_textarea').classList.remove('border-red')
+        document.querySelector('#excellent').style = 'display: block'
+        document.querySelector('#message').style.color = '#0a3871'
+        document.querySelector('#mistake').style = 'display: none'
+        
+        if( writeData.value == "" ) {
+
+            document.querySelector('#excellent').style = 'display: none'
+        }
+        
+    } else {
+        
+        document.querySelector('.form_textarea').classList.add('border-red')
+        document.querySelector('#mistake').style = 'display: block'
+        document.querySelector('#message').style.color = '#ff0000'
+        document.querySelector('#excellent').style = 'display: none'
+    }
+};
